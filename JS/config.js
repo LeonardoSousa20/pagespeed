d540d4e07df3238448bf6fed92514d48c1651eb9
Leonardo <leonardo.sousa7120@gmail.com>
2022-07-06 21:34:48 -0300

    sidebar toggle

--- JS/config.js
+++ JS/config.js
@@ -8,3 +8,21 @@ $("#resultado1").hide();
 $("#resultado2").hide();
 $("#resultado3").hide();
 $("#resultado4").hide();
+
+// SIDEBAR TOOGLE
+
+$("#sidebar-toggle").click(function () {
+  if ($(".side-bar").hasClass("open")) {
+    $(".sidebar-item-btn>span").hide("fast");
+    $(".side-bar").animate({ width: "67px" }, 100);
+    $(".side-bar").addClass("close");
+    $(".side-bar").removeClass("open");
+    $(".sidebar-toggle>svg").css("transform", "rotate(180deg)");
+  } else {
+    $(".sidebar-item-btn>span").show("fast");
+    $(".side-bar").animate({ width: "200px" }, 100);
+    $(".side-bar").addClass("open");
+    $(".side-bar").removeClass("close");
+    $(".sidebar-toggle>svg").css("transform", "");
+  }
+});
